6da64bc7cf157f3ba9db6c148672093cacccecce
alizamustafa <57371681+alizamustafa@users.noreply.github.com>
2019-11-05 02:04:43 +0500

    mustafa

---
+++ alizaaa.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+print("Twinkle twinkle little star! " "\n"
+          "how i wonder what you are " "\n"
+            "up above the world so high" "\n"
+            " like adimond in the sky" "\n"
+      "twinkle twinkle little star " "\n"
+      "how i wonder what u are")
+
+
+# In[8]:
+
+
+import sys
+print(sys.version)
+
+
+# In[10]:
+
+
+from datetime import datetime
+now=datetime.now()
+print(now)
+
+
+# In[12]:
+
+
+import math as m 
+radius=float(input("enter radius"))
+area=m.pi*(m.pow(radius,2))
+print("area is",area)
+
+
+# In[13]:
+
+
+a=input("enter ur first name")
+b=input("enter ur second name")
+c=b+" "+a
+print(c)
+
+
+# In[14]:
+
+
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+c=a+b
+print(c)
+
+
+# In[ ]:
+
+
+
+
